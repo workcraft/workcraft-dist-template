f1b1028138c167d1779e557ee41cf15aed7f01d1
Danil Sokolov <danilovesky@gmail.com>
2018-10-23 23:29:50 +0100

    Check presence of Java and correctness of version

--- windows/workcraft.bat
+++ windows/workcraft.bat
@@ -1,21 +1,24 @@
+@ECHO OFF
+
 :: Enable command line extensions
 VERIFY OTHER 2>NUL
 SETLOCAL ENABLEEXTENSIONS
 IF ERRORLEVEL 1 (
-    ECHO "Cannot enable command line extensions."
+    ECHO Cannot enable command line extensions.
+    PAUSE
     EXIT /B 1
 )
 
 :: Change to Workcraft home directory and put it into the WORKCRAFT_HOME variable
-:: (CD does not support UNC paths (those started with \\), and therefore
-:: PUSHD is used instead as it creates a network drive in case of UNC path)
-SET WORKCRAFT_HOME=%~dp0
+:: Note that CD does not support UNC paths (those started with \\). Therefore
+:: PUSHD should be used instead as it creates a network drive in case of UNC path.
+SET "WORKCRAFT_HOME=%~dp0"
 PUSHD "%WORKCRAFT_HOME%"
 
 :: SET the JVM executable in JAVA_BIN variable (if not defined yet)
 IF NOT DEFINED JAVA_BIN (
     IF NOT DEFINED JAVA_HOME (
-        SET JAVA_BIN=javaw.exe
+        SET "JAVA_BIN=javaw.exe"
     ) ELSE (
         IF EXIST "%JAVA_HOME%\bin\javaw.exe" (
             SET "JAVA_BIN=%JAVA_HOME%\bin\javaw.exe"
@@ -25,16 +28,32 @@ IF NOT DEFINED JAVA_BIN (
     )
 )
 
+:: Check presence of Java binary
+WHERE /Q "%JAVA_BIN%"
+IF ERRORLEVEL 1 (
+    ECHO Cannot find Java binary %JAVA_BIN%.
+    PAUSE
+    EXIT /B 1
+)
+
+:: Check correctness of Java version
+FOR /F tokens^=2-3^ delims^=.-_^" %%j IN ('%JAVA_BIN% -fullversion 2^>^&1') DO SET "JAVA_VERSION=%%j.%%k"
+IF NOT "%JAVA_VERSION%" == "1.8" (
+    ECHO Java 1.8 is required, but version %JAVA_VERSION% is found.
+    PAUSE
+    EXIT /B 1
+)
+
 :: Add tools\GraphvizMinimal\ to the path so tools\Petrify\draw_astg can find dot.exe
 SET "PATH=%PATH%;%WORKCRAFT_HOME%\tools\GraphvizMinimal\"
 
 SET "CLASSPATH=%WORKCRAFT_HOME%\bin\*"
 
-start "Workcraft" "%JAVA_BIN%" org.workcraft.Console %*
+START "Workcraft" "%JAVA_BIN%" org.workcraft.Console %*
 
-timeout 1 /nobreak
+TIMEOUT 1 /NOBREAK
 
-:: Release the network drives created by PUSHD and restore the current directory
+:: Rreleases the network drives created by PUSHD restores the current directory
 POPD
 
 ENDLOCAL
--- windows/workcraft.ps1
+++ windows/workcraft.ps1
@@ -1,5 +1,5 @@
 # Change to Workcraft home directory and put it into the WORKCRAFT_HOME variable
-# (Set-Location does not support UNC paths (those started with \\); and therefore
+# (Set-Location does not support UNC paths (those started with \\) and therefore
 # Push-Location is used instead as it creates a network drive in case of UNC path)
 $WORKCRAFT_HOME=$PSScriptRoot
 Push-Location $WORKCRAFT_HOME
@@ -17,10 +17,29 @@ if (! (Get-Variable 'JAVA_BIN' -Scope Global -ErrorAction 'Ignore')) {
     }
 }
 
+# Check presence of Java binary
+if ((Get-Command $JAVA_BIN -ErrorAction SilentlyContinue) -eq $null) {
+   Write-Host "Cannot find Java binary $JAVA_BIN"
+   Read-Host -Prompt "Press Enter to exit"
+   exit 1
+}
+
+# Check correctness of Java version
+$JAVA_VERSION=(Get-Command $JAVA_BIN | Select-Object -ExpandProperty Version).Major
+if ($JAVA_VERSION -ne 8) {
+   Write-Host "Java 8 is required, but version $JAVA_VERSION is found."
+   Read-Host -Prompt "Press Enter to exit"
+   exit 1
+}
+
 # Add tools\GraphvizMinimal\ to the path so tools\Petrify\draw_astg can find dot.exe
 $env:Path += ";$WORKCRAFT_HOME\tools\GraphvizMinimal\"
 
-Start-Process $JAVA_BIN -ArgumentList "-classpath $WORKCRAFT_HOME\bin\*", 'org.workcraft.Console', "$args" -Wait
+$ARGUMENT_LIST = @()
+$ARGUMENT_LIST += "-classpath $WORKCRAFT_HOME\bin\*"
+$ARGUMENT_LIST += 'org.workcraft.Console'
+$ARGUMENT_LIST += $args
+Start-Process $JAVA_BIN -ArgumentList $ARGUMENT_LIST -Wait
 
 # Release the network drives created by Push-Location and restore the current directory
 Pop-Location
