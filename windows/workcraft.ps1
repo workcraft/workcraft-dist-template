56932876d6316981ddd26a5d5278570f8aeb1a56
Danil Sokolov <danilovesky@gmail.com>
2023-04-11 16:59:26 +0100

    Fix PowerShell starter script for use of local JRE

--- windows/workcraft.ps1
+++ windows/workcraft.ps1
@@ -1,28 +1,28 @@
 # Remember the current working directory
-$CURRENT_DIR=Get-Location
+Set-Variable -Scope local -Name CURRENT_DIR -Value (Get-Location)
 
 # Change to Workcraft home directory and put it into the WORKCRAFT_HOME variable
 # (Set-Location does not support UNC paths (those started with \\) and therefore
 # Push-Location is used instead as it creates a network drive in case of UNC path)
-$WORKCRAFT_HOME=$PSScriptRoot
+Set-Variable -Scope local -Name WORKCRAFT_HOME -Value $PSScriptRoot
 Push-Location $WORKCRAFT_HOME
 
 # Use bundled JRE as the first priority (if present and not overridden)
 if (!  (Get-Variable 'WORKCRAFT_USE_NATIVE_JAVA' -Scope Global -ErrorAction 'Ignore')) {
     if (Test-Path $WORKCRAFT_HOME\jre\bin\javaw.exe) {
-        $JAVA_BIN="$WORKCRAFT_HOME\jre\bin\javaw.exe"
+        Set-Variable -Scope global -Name JAVA_BIN -Value "$WORKCRAFT_HOME\jre\bin\javaw.exe"
     }
 }
 
 # SET the JVM executable in JAVA_BIN variable (if not defined yet)
-if (! (Get-Variable 'JAVA_BIN' -Scope Global -ErrorAction 'Ignore')) {
-    if (! (Get-Variable 'JAVA_HOME' -Scope Global -ErrorAction 'Ignore')) {
-        $JAVA_BIN="javaw.exe"
+if (! (Get-Variable 'JAVA_BIN' -Scope global -ErrorAction 'Ignore')) {
+    if (! (Get-Variable 'JAVA_HOME' -Scope global -ErrorAction 'Ignore')) {
+        Set-Variable -Scope global -Name JAVA_BIN -Value "javaw.exe"
     } else {
         if (Test-Path $JAVA_HOME\bin\javaw.exe) {
-            $JAVA_BIN="$JAVA_HOME\bin\javaw.exe"
+            Set-Variable -Scope global -Name JAVA_BIN -Value "$JAVA_HOME\bin\javaw.exe"
         } else {
-             $JAVA_BIN="$JAVA_HOME\javaw.exe"
+            Set-Variable -Scope global -Name JAVA_BIN -Value "$JAVA_HOME\javaw.exe"
         }
     }
 }
