c054aa101ece138c11474592f867142a7d838a86
Danil Sokolov <danilovesky@gmail.com>
2026-09-07 08:47:02 +0100

    Add hook for removing ADDLib if unused

---
+++ common-intro.sh
@@ -0,0 +1,6 @@
+lib_path="${platform_path}/${workcraft}/${common}/lib"
+# ADDLib is currently unused in public distribution of Workcraft
+rm -rf ${lib_path}/addlib-api-*.jar
+rm -rf ${lib_path}/addlib-core-*.jar
+rm -rf ${lib_path}/addlib-cudd-*.jar
+rm -rf ${lib_path}/addlib-util-*.jar
